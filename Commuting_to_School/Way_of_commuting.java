b29533ae387a776a57f0594b772ae28cfc93771a
hi3036 <cssu24127@g.nihon-u.ac.jp>
2025-09-02 22:57:13 +0900

    boolean型では戻り値が足りないので、enumに変更

--- Commuting_to_School/Way_of_commuting.java
+++ Commuting_to_School/Way_of_commuting.java
@@ -3,7 +3,7 @@ package Commuting_to_School;
 import java.util.Scanner;
 
 class Original_Password {
-    private static String Password = "testPassword";
+    private static String Password = "firstPassword";
 
     public static String getPassword(String password) {
         //System.out.println("getPasswordが呼び出された");
@@ -35,13 +35,14 @@ class School_scanner {
         this.scanner = scanner;
     }
 
-    public boolean Input_password() {//パスワード入力処理
+    public Judge_Select Input_password() {//パスワード入力処理
         String input_text = scanner.nextLine();
         if (input_text.equals(Original_Password.getPassword(input_text))) {
-            return true;
+            return Judge_Select.True;
+        } else if (input_text.equals("cancel")) {
+            return Judge_Select.Cancel;
         } else {
-            if (input_text.equals("cancel"));//キャンセル処理 使う予定なし
-            return false;
+            return Judge_Select.False;
         }
     }
 
@@ -153,37 +154,54 @@ class Menu_operation {
                 4: ログアウト (Logout)
                 """);
     }
+
+    static void Human_List_serbice() {
+        System.out.println("""
+                名簿 (Main menue) 
+                1: メンバーおよび情報の追加
+                2: メンバーの編集
+                3: メインメニューに戻る
+                """);
+    }
 }
 
 
 public class Way_of_commuting {
     public static void main(String[] args) throws Mismatch_Normal_Exception {
-        
-        
-
+        run();
     }
 
-    public static void run() {
-
+    public static void run() throws Mismatch_Normal_Exception {
+        boolean Login_service_return;
+        do {
+            Login_service_return = Login_service();//ログインしつつ、falseが帰ってきたら最初に戻り、trueならば終了する
+        } while (!Login_service_return);
     }
 
-    public static void Login_service() throws Mismatch_Normal_Exception {//ログイン状態ですることをまとめる
+    public static boolean Login_service() throws Mismatch_Normal_Exception {//ログイン状態ですることをまとめる
         Menu_operation.Start_screen();
-        Login();
+        boolean return_Start_screen = Login();
+        if(!return_Start_screen) {//ログインせず、スタート画面にもどる処理
+            return false;
+        }
 
         Menu_operation.Open_service();
+        return true;
     }
 
-    public static void Login() throws Mismatch_Normal_Exception {
+    public static boolean Login() throws Mismatch_Normal_Exception {
         boolean Enter = false;
         do { 
             try {
                 School_scanner input = new School_scanner(new Scanner(System.in));
-                boolean Enter_judge =  input.Input_password();
+                //boolean Enter_judge =  input.Input_password();
+                School_scanner.Judge_Select Enter_judge =  input.Input_password();
 
-                if(Enter_judge) {
+                if(Enter_judge == School_scanner.Judge_Select.True) {
                     Enter = true;
-                } else {//Enter_judge = false
+                } else if (Enter_judge == School_scanner.Judge_Select.Cancel) {
+                    return false;//キャンセル
+                } else {//Enter_judge == School_scanner.Judge_Select.False
                     throw new Mismatch_Normal_Exception("""
 
                         パスワード入力に失敗しました. 入力が無効です. もう一度おねがいします.
@@ -199,6 +217,7 @@ public class Way_of_commuting {
         } while (!Enter);
         //System.out.println("Watch Enter value :"+Enter);
         System.out.println("パスワード入力に成功しました.");
+        return true;
     }
 
 
